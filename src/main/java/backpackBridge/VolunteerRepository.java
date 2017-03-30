438a0accbb78192ec553e1ab7fcdb9be8fdecf11
Dinah <umbohsbba@yahoo.com>
2017-03-30 16:10:31 -0400

    added volunteer java

---
+++ src/main/java/backpackBridge/VolunteerRepository.java
@@ -0,0 +1,9 @@
+package backpackBridge;
+
+import java.util.Set;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface VolunteerRepository extends CrudRepository<Student, Long> {
+
+}
